ab23f4450a36d85d1d79411d4148031d126f2bab
Zhiquan.Wang <mySabreHawk@outlook.com>
2018-09-30 19:25:22 -0700

    Complete HW1

---
+++ Homework-1/Homework-1/Script.R
@@ -0,0 +1,44 @@
+# Read Data 
+tmp_data = read.table("weight_full.txt", header = T);
+
+x_data = tmp_data$height;
+y_data = tmp_data$weight;
+
+
+# calculate values
+data_num = lengths(tmp_data[1]);
+mean_height = sum(tmp_data$height) / data_num;
+mean_weight = sum(tmp_data$weight) / data_num;
+cat("mean_x : ",mean_height,'\n')
+cat("mean_y : ",mean_weight,'\n')
+
+# plot 
+plot(x = x_data ,y = y_data, xlab = "Height", ylab = "Weight");
+reg = lm(y_data ~ x_data);
+abline(reg);
+points(mean_height, mean_weight, col = "blue");
+b1_up = 0;
+q2 = 0;
+q3 = 0;
+q4 = 0;
+for (i in 1:data_num) {
+    b1_up = b1_up + (tmp_data$height[i] - mean_height) * (tmp_data$weight[i] - mean_weight);
+    q2 = q2 + (x_data[i] - mean_height) * y_data[i];
+    q3 = q3 + x_data[i] * (y_data[i] - mean_weight);
+    q4 = q4 + x_data[i] * y_data[i];
+}
+
+b1_down = 0;
+for (i in 1:data_num) {
+    b1_down = b1_down + (tmp_data$height[i] - mean_height) ^ 2;
+}
+b1 = b1_up / b1_down;
+cat("b1 : ",b1,'\n')
+b0 = mean_weight - b1 * mean_height;
+cat("b0 : ",b0,'\n')
+
+cat("q1 : ", b1_up, '\n');
+cat("q2 : ", q2, '\n');
+cat("q3 : ", q3, '\n');
+cat("q4 : ", q4, '\n');
+
