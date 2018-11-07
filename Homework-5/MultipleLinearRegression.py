10cd2d475198b359ed3c3405141e683e6c40756a
Zhiquan.Wang <mySabreHawk@outlook.com>
2018-11-06 21:02:04 -0800

    Homework-5

---
+++ Homework-5/MultipleLinearRegression.py
@@ -0,0 +1,121 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+__author__ = 'Zhiquan Wang'
+__date__ = '2018/11/5 15:25'
+
+import copy
+import math
+import numpy as np
+from scipy.stats import f
+from scipy.stats import t
+
+
+class MultipleLinearRegression(object):
+    def __init__(self):
+        self.__sample_num = 0
+        self.__predictor_num = 0
+        self.__x_mat = None
+        self.__design_mat = None
+        self.__y_mat = None
+        self.__beta_hat_mat = None
+        self.__hat_mat = None
+        self.__y_hat_mat = None
+        self.__error_mat = None
+        self.__sigma_square_hat = None
+        self.__variance_beta_hat = None
+
+    def load_data(self, *, file_name):
+        tmp_data = np.loadtxt(file_name)
+        self.__y_mat = np.mat(tmp_data[:, 0]).T
+        self.__x_mat = np.mat(tmp_data[:, 1:])
+        self.__sample_num = len(self.__y_mat)
+        self.__design_mat = np.hstack((np.ones((self.__sample_num, 1)), self.__x_mat))
+        self.__predictor_num = self.__design_mat.shape[1]
+
+    def init_model(self):
+        self.__beta_hat_mat = (self.__design_mat.T * self.__design_mat).I * self.__design_mat.T * self.__y_mat
+        self.__hat_mat = self.__design_mat * (self.__design_mat.T * self.__design_mat).I * self.__design_mat.T
+        self.__y_hat_mat = self.__hat_mat * self.__y_mat
+        self.__error_mat = self.__y_mat - self.__y_hat_mat
+        self.__sigma_square_hat = float((self.__error_mat.T * self.__error_mat) / (
+                self.__sample_num - self.__predictor_num))
+        self.__variance_beta_hat = self.__sigma_square_hat * (self.__design_mat.T * self.__design_mat).I
+
+    def variance_y_hat(self, *, x_list):
+        x_hat_mat = np.mat(x_list).T
+        return self.__sigma_square_hat * x_hat_mat.T * (self.__design_mat.T * self.__design_mat).I * x_hat_mat
+
+    def hypothesis_test_zero(self, *, index_list, alpha):
+        sse_full = float(self.__error_mat.T * self.__error_mat)
+        tmp_design_mat = np.delete(self.__design_mat, index_list, axis=1)
+        tmp_beta_mat = (tmp_design_mat.T * tmp_design_mat).I * tmp_design_mat.T * self.__y_mat
+        tmp_yr_hat_mat = tmp_design_mat * tmp_beta_mat
+        tmp_error_mat = self.__y_mat - tmp_yr_hat_mat
+        sse_residual = tmp_error_mat.T * tmp_error_mat
+        df_sse_f = self.__sample_num - self.__predictor_num
+        df_sse_r = df_sse_f + len(index_list)
+        tmp_value = float(((sse_residual - sse_full) / (df_sse_r - df_sse_f)) / (sse_full / df_sse_f))
+        f_value = f.isf(alpha, df_sse_r - df_sse_f, df_sse_f)
+        ht = tmp_value < f_value
+        return [ht, tmp_value, f_value]
+
+    def confidence_interval_y_hat(self, *, x_list, alpha):
+        tmp_x_mat = np.mat(x_list).T
+        pre_num = tmp_x_mat.shape[1]
+        tmp_y_hat = tmp_x_mat.T * self.__beta_hat_mat
+        t_value = t.isf(alpha / (2 * pre_num), self.__sample_num - self.__predictor_num)
+        tmp_sd = np.sqrt(self.variance_y_hat(x_list=x_list))
+        return [np.diag(tmp_y_hat - t_value * tmp_sd), np.diag(tmp_y_hat + t_value * tmp_sd)]
+
+    def prediction_interval_y_hat(self, *, x_list, alpha):
+        tmp_x_mat = np.mat(x_list).T
+        pre_num = tmp_x_mat.shape[1]
+        tmp_y_hat = tmp_x_mat.T * self.__beta_hat_mat
+        t_value = t.isf(alpha / (2 * pre_num), self.__sample_num - self.__predictor_num)
+        tmp_sd = np.sqrt(self.__sigma_square_hat * (
+                1 + tmp_x_mat.T * (self.__design_mat.T * self.__design_mat).I * tmp_x_mat))
+        return np.mat([np.diag(tmp_y_hat - t_value * tmp_sd),np.diag(tmp_y_hat + t_value * tmp_sd)]).T
+
+    @property
+    def sample_num(self):
+        return self.__sample_num
+
+    @property
+    def predictor_num(self):
+        return self.__predictor_num
+
+    @property
+    def x_mat(self):
+        return self.__x_mat
+
+    @property
+    def y_mat(self):
+        return self.__y_mat
+
+    @property
+    def design_mat(self):
+        return self.__design_mat
+
+    @property
+    def beta_hat_mat(self):
+        return self.__beta_hat_mat
+
+    @property
+    def hat_mat(self):
+        return self.__hat_mat
+
+    @property
+    def y_hat_mat(self):
+        return self.__y_hat_mat
+
+    @property
+    def error_mat(self):
+        return self.__error_mat
+
+    @property
+    def sigma_square_hat(self):
+        return self.__sigma_square_hat
+
+    @property
+    def variance_beta_hat(self):
+        return self.__variance_beta_hat
---
+++ Homework-5/main.py
@@ -0,0 +1,31 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+__author__ = 'Zhiquan Wang'
+__date__ = '2018/11/5 15:24'
+
+import MultipleLinearRegression as mlr
+import numpy as np
+
+if __name__ == '__main__':
+    tmp_mode = mlr.MultipleLinearRegression()
+    tmp_mode.load_data(file_name='property.txt')
+    tmp_mode.init_model()
+    np.set_printoptions(precision=4, suppress=True)
+    print('The dimension of the design matrix X =', tmp_mode.design_mat.shape)
+    print('The dimension of the response matrix Y =', tmp_mode.y_mat.shape)
+    print("X^t * X =\n", tmp_mode.design_mat.T * tmp_mode.design_mat)
+    print("X^t * Y =\n", tmp_mode.design_mat.T * tmp_mode.y_mat)
+    print("(X^t * X)^-1 =\n", (tmp_mode.design_mat.T * tmp_mode.design_mat).I)
+    print("Beta hat =\n", tmp_mode.beta_hat_mat)
+    print("The first 6 cases of fitted values = \n", tmp_mode.y_hat_mat[0:6])
+    print("The first 6 cases of residuals = \n", tmp_mode.error_mat[0:6])
+    print("Estimate of sigma square =\n", np.mat(tmp_mode.sigma_square_hat))
+    # print("Variance of beta hat =\n",tmp_mode.variance_beta_hat)
+    print('Hypothesis test beta2 =0,signifiacne level = 0.01\n',
+          tmp_mode.hypothesis_test_zero(index_list=[2], alpha=0.01))
+    print('CI for x=[1,4,10,10,8],alpha = 0.05\n',
+          tmp_mode.confidence_interval_y_hat(x_list=[[1, 4, 10, 10, 8]], alpha=0.05))
+    print('PI for x=[1,4,10,10,8],alpha = 0.05\n',
+          tmp_mode.prediction_interval_y_hat(x_list=[[1, 4, 10, 10, 8]], alpha=0.05))
+    print('PI for x=[1,4,10,10,8],[1,5,11,8,12],alpha = 0.05\n',
+          tmp_mode.prediction_interval_y_hat(x_list=[[1, 4, 10, 10, 8], [1, 5, 11, 8, 12]], alpha=0.05))
