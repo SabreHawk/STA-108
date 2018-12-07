94d9019d9805adfdd25e2148809b0d7a29de0f3e
Zhiquan.Wang <mySabreHawk@outlook.com>
2018-12-06 16:22:48 -0800

    add b

--- Homework-7/RProject1/RProject1/Script.R
+++ Homework-7/RProject1/RProject1/Script.R
@@ -27,5 +27,25 @@ p = rowSums(summary(sel)$which)
 AIC = n * log(SSE) + 2 * p
 BIC = n * log(SSE) + p * log(n)
 
-model1 = lm(Y~X1)
-press2 = sum((model1$residuals / (1 - lm.influence(model1)$hat)) ^ 2)
+model1 = lm(mydata$Y ~ mydata$X1)
+press1 = sum((model1$residuals / (1 - lm.influence(model1)$hat)) ^ 2)
+
+model2 = lm(mydata$Y ~ mydata$X2)
+press2 = sum((model2$residuals / (1 - lm.influence(model2)$hat)) ^ 2)
+
+model3 = lm(mydata$Y ~ mydata$X3)
+press3 = sum((model3$residuals / (1 - lm.influence(model3)$hat)) ^ 2)
+
+model12 = lm(mydata$Y ~ mydata$X1 + mydata$X2)
+press12 = sum((model12$residuals / (1 - lm.influence(model12)$hat)) ^ 2)
+
+model13 = lm(mydata$Y ~ mydata$X1 + mydata$X3)
+press13 = sum((model13$residuals / (1 - lm.influence(model13)$hat)) ^ 2)
+
+model23 = lm(mydata$Y ~ mydata$X2 + mydata$X3)
+press23 = sum((model23$residuals / (1 - lm.influence(model23)$hat)) ^ 2)
+
+model123 = lm(mydata$Y ~ mydata$X1 + mydata$X2 + mydata$X3)
+press123 = sum((model123$residuals / (1 - lm.influence(model123)$hat)) ^ 2)
+
+
